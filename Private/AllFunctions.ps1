32e6adf495aca673d85acb4db3fcccdf5fdcc1f8
zottelbeyer <zottelbeyer@gmail.com>
2024-11-26 17:31:23 +0100

    Workaround for RPC/DISM error on Win11 22H2 / Win11 24H2

--- Private/AllFunctions.ps1
+++ Private/AllFunctions.ps1
@@ -6705,9 +6705,11 @@ function Update-CumulativeOS {
         Write-Host -ForegroundColor Cyan "INSTALLING        " -NoNewline
         Write-Host -ForegroundColor Gray "$($Update.Title) - $(Split-Path $Update.OriginUri -Leaf)"
 
-        # Workaround for RPC/DISM error on Win11 22H2
+        # Workaround for RPC/DISM error on Win11 22H2 / Win11 24H2
         $TestWindowsPackageCAB = $null
         if ($OSImageName -like '*Windows 11*' -and $ReleaseID -eq '22H2') {
+            $TestWindowsPackageCAB = 'CombinedMSU'
+        }elseif ($OSImageName -like '*Windows 11*' -and $ReleaseID -eq '24H2') {
             $TestWindowsPackageCAB = 'CombinedMSU'
         }
         else {
